1f808c8b14bf1d9cd26328cad19666d62f23f062
1334 <1334@shadowproject.net>
2012-04-13 14:08:58 +0200

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intranet::Application.config.secret_token = '154036871a8b704f41728103e82bde1f6245d90f29739ac59bbdbbaa7006eb38768e5aba9cee7ad7d8db86d7463803f59c988a00fbf98fc873598ca64d84396b'
