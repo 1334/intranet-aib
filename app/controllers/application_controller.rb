6c0efee56180b2df1a1e5143a9f0eebfa11825a3
1334 <1334@shadowproject.net>
2012-05-22 17:39:12 +0200

    Added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authorize, except: [:login, :index]
+
   private
 
   def current_user
@@ -8,4 +10,8 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user #makes this method available from the views
+
+  def authorize
+    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
 end
