3b29fb837eac64cd0ab0e382f86484a1c033cf10
1334 <1334@shadowproject.net>
2012-11-23 00:07:10 +0100

    remove data json from jason response

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
     
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @projects }
+      format.json { render json: @projects.to_json(only: [:code, :name]) }
     end
   end
 
