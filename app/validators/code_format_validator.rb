1709fc66b23d7b86fafbe3a4bedc5c7b737071a8
1334 <1334@shadowproject.net>
2012-11-22 18:55:31 +0100

    extend initials length from 4 to 6 chars

--- app/validators/code_format_validator.rb
+++ app/validators/code_format_validator.rb
@@ -1,6 +1,6 @@
 class CodeFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /^(\d{3}\S{2,4})$/i
+    unless value =~ /^(\d{3}\S{2,6})$/i
       object.errors[attribute] << (options[:message] || "is not a valid code format")
     end
   end
