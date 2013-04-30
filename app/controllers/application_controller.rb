e9e22385b6587eeb2536a57867ef83b3f7321eef
1334 <1334@shadowproject.net>
2013-04-30 13:26:25 +0200

    tighten visibility b4 authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authorize, except: [:login, :index, :show]
+  before_filter :authorize, except: :login
   before_filter :set_locale
 
   private
